4d2a772edb87fac2a91e09548b11913d5e41f73a
Vladimir <lianino@mail.ru>
2017-09-16 12:06:17 +0700

    ex. 1.4-1.6

--- Lazhoye/main.cpp
+++ Lazhoye/main.cpp
@@ -2,13 +2,23 @@
 
 int	main() 
 {
-	std::cout << "Hello, World" << std::endl;
+	//std::cout << "Hello, World" << std::endl;
 	
-	/*std::cout << "Enter two numbers:" << std::endl;
+	std::cout << "Enter two numbers:" << std::endl;
 
 	int	v1 = 0, v2 = 0;
 	std::cin >> v1 >> v2;
 
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
-	return	0; */
+	// std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+
+	std::cout << "The multiplication of ";
+	std::cout << v1;
+	std::cout << " and ";
+	std::cout << v2;
+	std::cout << " is ";
+	std::cout << v1 * v2 << std::endl;
+
+
+
+	return	0; 
 }
